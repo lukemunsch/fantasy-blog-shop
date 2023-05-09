bfeff9a529c5448c0c3f1304eac006ac4a6f3cb6
Luke Munsch <lpmunsch@gmail.com>
2023-05-09 23:04:22 +0100

    Add new admin for personnel model

--- personnel/admin.py
+++ personnel/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Personnel
 
 # Register your models here.
+@admin.register(Personnel)
+class PersonnelAdmin(SummernoteModelAdmin):
+    """set up our personnel creation form"""
+    prepopulated_fields = {'slug': ('name', 'member_id',)}
+    summernote_field = ['speciality']
+
+    list_filter = ('rank', 'current_status')
+    list_display = (
+        'name',
+        'age',
+        'rank',
+        'current_status',
+    )
+    search_fields = [
+        'name',
+        'originated_from',
+        'speciality',
+    ]
