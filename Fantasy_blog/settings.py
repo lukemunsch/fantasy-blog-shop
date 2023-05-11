08fa5335da2ac106b3b1741b91efbaadafca903e
Luke Munsch <lpmunsch@gmail.com>
2023-05-11 22:33:55 +0100

    Save changes for settings

--- Fantasy_blog/settings.py
+++ Fantasy_blog/settings.py
@@ -146,7 +146,7 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'loreynas-ticket-counter'
+    AWS_STORAGE_BUCKET_NAME = 'fantasy-blog-shop'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
