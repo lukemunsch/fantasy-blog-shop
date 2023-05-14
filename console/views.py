bf31c438a9dce5a199af8f50c517373785ad8806
Luke Munsch <lpmunsch@gmail.com>
2023-05-14 20:24:44 +0100

    update static files content

--- console/views.py
+++ console/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+from personnel.models import Personnel
+
 # Create your views here.
 def console(request):
     """set up new page for displaying all content"""
-    context = {}
+    crew = Personnel.objects.filter()[:5]
+    context = {
+        'crew': crew,
+    }
+
     return render(request, 'console/console.html', context)
