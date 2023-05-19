dfe2f295b9f6b2530a6cb4d3d1cc248f834060ca
Luke Munsch <lpmunsch@gmail.com>
2023-05-19 10:08:54 +0100

    Update news app html to copy base

--- news/views.py
+++ news/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-
 def news(request):
-    """set up our view for news updates"""
-    return render(request, 'news/news.html, context')
+    """set up your news updates view"""
+    return render(request, 'news/news.html')
