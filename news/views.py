9ccfc5b29e27ba771df5fe13151b476270fd42c5
Luke Munsch <lpmunsch@gmail.com>
2023-05-19 12:42:31 +0100

    Add new settings for news migrations and views

--- news/views.py
+++ news/views.py
@@ -5,8 +5,8 @@ from .models import News
 # Create your views here.
 def news(request):
     """set up your news updates view"""
-    
+    news = News.objects.all()
     context = {
-        
+        'news': news,
     }
     return render(request, 'news/news.html', context)
