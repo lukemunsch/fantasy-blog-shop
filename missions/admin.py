d242f4225bfe54741b906856d4328792305869d5
Luke Munsch <lpmunsch@gmail.com>
2023-05-23 21:50:04 +0100

    Add new model and admin for missions

--- missions/admin.py
+++ missions/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import Mission
+
 # Register your models here.
+@admin.register(Mission)
+class MissionAdmin(admin.ModelAdmin):
+    list_display = ('mission', 'mission_grade', 'mission_lead', 'mission_length',)
+
+    list_filter = (
+        'mission_grade',
+        'mission_lead',
+    )
+
+    search_fields = [
+        'mission',
+        'description',
+    ]
--- missions/models.py
+++ missions/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+from personnel.models import Personnel
+
 # Create your models here.
+GRADES = (
+    (1, 'Extreme'),
+    (2, 'Dangerous'),
+    (3, 'Serious'),
+    (4, 'Moderate'),
+    (5, 'Basic'),
+    (6, 'Training')
+)
+class Mission(models.Model):
+    """Set up our model for missions"""
+    mission = models.CharField(max_length=100, null=False, blank=False, unique=True)
+    mission_grade = models.IntegerField(choices=GRADES, default=5)
+    mission_lead = models.ForeignKey(Personnel, on_delete=models.Cascade)
+    description = models.TextField(max_length=2000, null=False, blank=False)
+    prep_time = models.PositiveIntegerField(max_length=2, default=5) #number in days
+    mission_length = models.PositiveIntegerField(max_length=1, default=1) #number in weeks
+
+    class Meta:
+        """Set up our extra model settings"""
+        ordering = ['grade']
+
+    def __str__(self):
+        return self.mission
