9f722f1ba51fd8b6452b8300e9ad06c159c3a676
Luke Munsch <lpmunsch@gmail.com>
2023-05-24 21:08:28 +0100

    Updating console to display entries

--- console/views.py
+++ console/views.py
@@ -2,15 +2,19 @@ from django.shortcuts import render
 
 from personnel.models import Personnel
 from news.models import News
+from missions.models import Mission
 
 # Create your views here.
 def console(request):
     """set up new page for displaying all content"""
     crew = Personnel.objects.filter()[:5]
     news = News.objects.filter()[:5]
+    mission = Mission.objects.filter()[:5]
+
     context = {
         'crew': crew,
         'news': news,
+        'mission': mission,
     }
 
     return render(request, 'console/console.html', context)
