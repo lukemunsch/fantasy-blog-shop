013ad4b0db711e42c33b5f1aff2a82b0553a671a
Luke Munsch <lpmunsch@gmail.com>
2023-05-24 20:53:17 +0100

    Update mission.html to display templates

--- missions/views.py
+++ missions/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from .models import Mission
 # Create your views here.
 def missions(request):
     """set up our standard missions page"""
-    return render(request, 'missions/missions.html')
+    mission = Mission.objects.all()
+
+    context = {
+        'mission': mission,
+    }
+
+    return render(request, 'missions/missions.html', context)
