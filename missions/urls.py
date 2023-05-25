685d957b69f2abe8e27d8cf988f0fd4d55712082
Luke Munsch <lpmunsch@gmail.com>
2023-05-25 21:37:40 +0100

    Update links for entry details drilldowns

--- missions/urls.py
+++ missions/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.missions, name='missions')
+    path('', views.missions, name='missions'),
+    path('<int:mission_id>', views.mission_details, name='mission_details'),
 ]
--- missions/views.py
+++ missions/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect, get_object_or_404
 from .models import Mission
 # Create your views here.
 def missions(request):
@@ -10,3 +10,13 @@ def missions(request):
     }
 
     return render(request, 'missions/missions.html', context)
+
+def mission_details(request, mission_id):
+    """Set up how we display our mission brief"""
+    task = get_object_or_404(Mission, pk=mission_id)
+
+    context = {
+        'task': task,
+    }
+
+    return render(request, 'missions/missions-details.html', context)
