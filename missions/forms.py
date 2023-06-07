ec656e22dbd0bf1541399c356361668171efe9af
Luke Munsch <lpmunsch@gmail.com>
2023-06-07 22:18:04 +0100

    Upload new files form forms and add pages

--- missions/forms.py
+++ missions/forms.py
@@ -1,14 +1,33 @@
 from django import forms
 
 from .models import Mission
+from personnel.models import Personnel
 from .widgets import CustomClearableFileInput
+
+GRADES = (
+    (1, 'Extreme'),
+    (2, 'Dangerous'),
+    (3, 'Serious'),
+    (4, 'Moderate'),
+    (5, 'Basic'),
+    (6, 'Training')
+)
+STATUS = (
+    (1, 'Active'),
+    (2, 'Completed'),
+    (3, 'On Hold'),
+    (4, 'Cancelled')
+)
+PUBLISH = ((0, 'Hidden'), (1, 'Displayed'))
 
 class MissionForm(forms.ModelForm):
     """
     Form for creating a new mission to go 
     into our pending list
     """
+    personnel = Personnel.objects.filter(rank>=2)
     class Meta:
+        """set up our form widgets"""
         model = Mission
         fields = [
             'mission',
@@ -16,7 +35,32 @@ class MissionForm(forms.ModelForm):
             'mission_lead',
             'prep_time',
             'mission_length',
-            'mission_status',
             'mission_img',
             'img_url',
+            'description',
         ]
+        widgets = {
+            'mission': forms.TextInput(
+                attrs={'placeholder': 'Mission Title Here'}
+            ),
+            'img_url': forms.TextInput(
+                attrs={'placeholder': 'Online Image URL'}
+            ),
+        }
+
+    mission_grade = forms.RadioSelect(choices=GRADES)
+    mission_lead = forms.ModelChoiceField(
+        queryset=personnel
+    )
+    prep_time = forms.NumberInput(
+        label='Prep Time (in days)'
+    )
+    mission_length = forms.NumberInput(
+        label='Mission Length (in weeks)'
+    )
+    mission_img = forms.ImageField(
+        label='Image Upload'
+    )
+    description = forms.Textarea(
+        placeholder='Add Description Here'
+    )
--- missions/views.py
+++ missions/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, reverse, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from .models import Mission
+from .forms import MissionForm
+
 # Create your views here.
 def missions(request):
     """set up our standard missions page"""
@@ -31,3 +33,31 @@ def mission_details(request, mission_id):
 @login_required
 def add_mission(request):
     """set up our new view for adding a new mission"""
+    if not request.user.is_superuser:
+        message.error(
+            request,
+            'Sorry, You cannot access this function!'
+        )
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = MissionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            message.success(
+                request,
+                'New Mission is waiting your approval!'
+            )
+            return redirect(reverse('home'))
+        else:
+            messages.error(
+                request,
+                'Failed to create Mission - Ensure all details are correct!'
+            )
+    else:
+        form = MissionForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'mission/add-mission.html', context)
