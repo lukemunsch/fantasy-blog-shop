ca58ae029c4bc677aeb020aaf8ff3486dd7c185e
Luke Munsch <lpmunsch@gmail.com>
2023-06-26 21:17:01 +0100

    Updating views for correct profiles display

--- profiles/views.py
+++ profiles/views.py
@@ -2,14 +2,29 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
-# Create your views here.
+
 def profile(request):
-    """set up our view for the profiles"""
+    """display the user's profle"""
     profile = get_object_or_404(UserProfile, user=request.user)
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile Successfully Updated')
+        else:
+            messages.error(request, (
+                'Update failed. Please ensure '
+                'the form is valid.'))
+    else:
+        form = UserProfileForm(instance=profile)
 
-    context={
+    template = 'profiles/profile.html'
+    context = {
         'profile': profile,
+        'form': form,
+        'on_profile_page': True,
     }
 
-    return render(request, 'profiles/profile.html', )
+    return render(request, template, context)
