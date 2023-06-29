6d2607ae482fc2a1f3123d73c2aae52f1b45569e
Luke Munsch <lpmunsch@gmail.com>
2023-06-29 20:06:15 +0100

    Add new model and form and migrations for profiles

---
+++ profiles/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    """set up our form for user profiles"""
+    class Meta:
+        """forms specifics set up"""
+        model = UserProfile
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_phone_number': 'Phone Number',
+            'default_town_or_city': 'Town or City',
+        }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
