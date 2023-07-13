60b2611c9ce166111c1a9951be4d2a45c2e7780a
Luke Munsch <lpmunsch@gmail.com>
2023-07-13 21:44:52 +0100

    Add new Product model and migrations

--- resources/models.py
+++ resources/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
+CATEGORY = ((0, 'Mineral'), (1, 'Food'), (2, 'Material'), (3, 'Fabric'))
+PUBLISH = ((0, 'Hidden'), (1, 'Displayed'))
+class Products(models.Model):
+    """set up model for our resources"""
+    name = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+        unique=True
+    )
+    description = models.TextField(max_length=200, null=False, blank=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.PositiveIntegerField(choices=CATEGORY, default=0)
+    approved_item = models.PositiveIntegerField(choices=PUBLISH, default=0)
+    slug = models.SlugField(max_length=254, unique=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(f'{self.category}-{self.name}')
+        return super().save(*args, **kwargs)
