d2e4a9bc717cc99ebf14c6204028fdf596a9deb8
Luke Munsch <lpmunsch@gmail.com>
2023-07-14 12:05:29 +0100

    Add new code for displaying categories on shop

--- resources/admin.py
+++ resources/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Category
+
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    """set up our display in admin for categories"""
+    list_display = ('name', 'friendly_name',)
+
+admin.site.register(Category, CategoryAdmin)
--- resources/views.py
+++ resources/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 
-from .models import Products
+from .models import Products, Category
 
 # Create your views here.
 def resources(request):
     """set up shop for our resources"""
+    categories = Category.objects.all()
     products = Products.objects.all()
 
     context = {
         'products': products,
+        'categories': categories,
     }
     return render(request, 'resources/resources.html', context)
