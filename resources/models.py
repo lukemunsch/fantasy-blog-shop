4e880c7f4028a6f1ddfacf63fb5c863aa91b16cd
Luke Munsch <lpmunsch@gmail.com>
2023-07-14 11:39:40 +0100

    add new model for categories and migrations

--- resources/models.py
+++ resources/models.py
@@ -2,8 +2,26 @@ from django.db import models
 from django.template.defaultfilters import slugify
 
 # Create your models here.
-CATEGORY = ((0, 'Mineral'), (1, 'Food'), (2, 'Material'), (3, 'Fabric'))
+
 PUBLISH = ((0, 'Hidden'), (1, 'Displayed'))
+
+class Category(models.Model):
+    """set up our model for th categories of our"""
+    name = models.CharField(max_length=50, null=False, blank=False)
+    friendly_name = models.CharField(max_length=50, null=False, blank=False)
+
+    class Meta:
+        """set up extra model settings"""
+        verbose_name_plural = 'Categories'
+
+    def __str__(self) -> str:
+        return self.name
+
+    def get_friendly_name(self):
+        """function for returning the 
+        friendly name instead of original name"""
+        return self.friendly_name
+
 class Products(models.Model):
     """set up model for our resources"""
     name = models.CharField(
@@ -14,7 +32,7 @@ class Products(models.Model):
     )
     description = models.TextField(max_length=200, null=False, blank=False)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.PositiveIntegerField(choices=CATEGORY, default=0)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     approved_item = models.PositiveIntegerField(choices=PUBLISH, default=0)
     slug = models.SlugField(max_length=254, unique=True)
     image = models.ImageField(null=True, blank=True)
@@ -23,6 +41,7 @@ class Products(models.Model):
         return self.name
 
     def save(self, *args, **kwargs):
+        """overriding the save function to create our slug"""
         if not self.slug:
             self.slug = slugify(f'{self.category}-{self.name}')
         return super().save(*args, **kwargs)
--- resources/views.py
+++ resources/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+from .models import Products
+
 # Create your views here.
 def resources(request):
     """set up shop for our resources"""
-    return render(request, 'resources/resources.html')
+    products = Products.objects.all()
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'resources/resources.html', context)
