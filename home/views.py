78135cda32becd28132510b60331d181ee8bb782
Luke Munsch <lpmunsch@gmail.com>
2023-07-26 20:34:03 +0100

    Update home page code for pending items

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 from missions.models import Mission, Update
 from news.models import News
 from personnel.models import Personnel
+from resources.models import Product
 
 # Create your views here.
 def index(request):
@@ -14,12 +15,14 @@ def index(request):
     articles = News.objects.filter(approved_post=0)
     members = Personnel.objects.filter(authorised=0)
     updates = Update.objects.filter(approved=0)
+    resources = Product.objects.filter(approved_item=0)
 
     context = {
         'missions': missions,
         'articles': articles,
         'members': members,
         'updates': updates,
+        'resources': resources,
     }
 
     return render(request, 'home/index.html', context)
