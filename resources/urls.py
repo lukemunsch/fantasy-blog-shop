4e3e7bf20d0aa1ce94641e840d8c03ee90cfacf2
Luke Munsch <lpmunsch@gmail.com>
2023-08-02 20:10:30 +0100

    Fix issue with url ending slash

--- resources/urls.py
+++ resources/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.resources, name='resources'),
     path('pending-resources/', views.pending_resources, name='pending_resources'),
-    path('resource-details/<slug:slug>', views.resource_details, name='resource_details'),
+    path('resource-details/<slug:slug>/', views.resource_details, name='resource_details'),
 ]
