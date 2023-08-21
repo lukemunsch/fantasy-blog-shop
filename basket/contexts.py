8b64a45e654358cdb792994ed9d6f7e936cc44fa
Luke Munsch <lpmunsch@gmail.com>
2023-08-21 21:36:46 +0100

    Update context and basket.html for content

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,15 +1,26 @@
 """import context requirements"""
 from decimal import Decimal
+from django.shortcuts import get_object_or_404
 from django.conf import settings
 
+from resources.models import Product
 
 def basket_contents(request):
     """set up a context file for basket"""
     basket_items = []
     total = 0
     product_count = 0
-    delivery = 0
     basket = request.session.get('basket', {})
+
+    for product_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=product_id)
+        total += quantity * product.price
+        product_count += quantity
+        basket_items.append({
+            'product_id': product_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.PURCHASE_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERC/100)
@@ -24,10 +35,11 @@ def basket_contents(request):
         'basket_items': basket_items,
         'total': total,
         'product_count': product_count,
+        'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'purchase_threshold': settings.PURCHASE_THRESHOLD,
         'grand_total': grand_total,
         'standard_delivery_perc': settings.STANDARD_DELIVERY_PERC,
     }
-    print(request.session.get('basket', {}))
+
     return context
