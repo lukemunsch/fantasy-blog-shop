a01030f21c872fddaff690968b95da4d7d8ea369
Luke Munsch <lpmunsch@gmail.com>
2023-08-25 12:23:04 +0100

    Update contexts file

--- basket/contexts.py
+++ basket/contexts.py
@@ -7,20 +7,9 @@ from resources.models import Product
 
 def basket_contents(request):
     """set up the context for our wallet"""
-    basket_products = []
+    basket_items = []
     total = 0
     product_count = 0
-    basket = request.session.get('basket', {})
-
-    for product_id, quantity in basket.items():
-        product = get_object_or_404(Product, pk=product_id)
-        total += quantity * product.price
-        product_count += quantity
-        basket_products.append({
-            'product_id': product_id,
-            'quantity': quantity,
-            'product': product,
-        })
 
     if total < settings.PURCHASE_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERC/100)
@@ -32,13 +21,13 @@ def basket_contents(request):
     grand_total = total + delivery
 
     context = {
-        'basket_products': basket_products,
+        'basket_items': basket_items,
         'total': total,
         'product_count': product_count,
-        'delivery': delivery,
-        'free_delivery_delta': free_delivery_delta,
         'PURCHASE_THRESHOLD': settings.PURCHASE_THRESHOLD,
+        'delivery': delivery,
         'STANDARD_DELIVERY_PERC': settings.STANDARD_DELIVERY_PERC,
+        'free_delivery_delta': free_delivery_delta,
         'grand_total': grand_total,
     }
 
