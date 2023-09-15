344df3c82232ee466bed00f4605ae631024597f5
Luke Munsch <lpmunsch@gmail.com>
2023-09-15 13:41:22 +0100

    Add new functionality for checkout app

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from checkout.models import Order, OrderLineItem
+
 # Register your models here.
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    """set up the admin for order model"""
+    readonly_fields = ('order_number', 'date', 'delivery_cost',
+                       'order_total', 'grand_total',)
+    inlines = (OrderLineItemAdminInline,)
+    fields = ('order_number', 'date', 'full_name', 'email', 'phone_number',
+              'town_or_city', 'street_address1', 'street_address2', 'county',
+              'delivery_cost', 'order_total', 'grand_total',)
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost', 'grand_total',)
+
+admin.site.register(Order, OrderAdmin)
---
+++ checkout/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    """set up the form for orders"""
+    class meta:
+        """set up the specifics"""
+        model = Order
+        fields = (
+            'full_name', 'email', 'phone_number', 'street_address1', 
+            'street_address2', 'town_or_city', 'postcode', 'country', 
+            'county',
+        )
+
+    def __init__(self, *args, **kwargs):
+        """add placeholders and classes, remove auto-generated
+        labels and set autofocus"""
+        super()._init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email',
+            'phone_number': 'Phone Number',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+            'country': 'Country',
+            'county': 'County',
+            'town_or_city': 'Town Or City',
+            'postcode': 'Postcode',
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus']=True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholder[field]} *'
+            else:
+                placeholder = placeholder[field]
+            self.fields[field].widget.attrs['placeholder']=placeholder
+            self.fields[field].widget.attrs['class']='stripe-style-input'
+            self.fields[field].label=False
