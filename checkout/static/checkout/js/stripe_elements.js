fb6026dd08a5cbf8645298e69e346fd166434224
Luke Munsch <lpmunsch@gmail.com>
2023-09-21 16:59:19 +0100

    Add new stripe functionality for checkout

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,37 @@
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
+var elements = stripe.elements();
+
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Urbanist", Arial, Helvetica, sans-serif, serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4',
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545',
+    }
+};
+
+var card = elements.create('card', {style: style});
+card.mount('#card-element');
+
+// handle realtime errors in validation on the card element
+card.addEventListener('change', function(event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert"><i class="fas fa-times"></i></span>
+            <span>${event.error.message}</span>
+        `
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
