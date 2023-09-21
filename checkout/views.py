60cc5fbf1ff2d0daf523001f6085d8f5568110f4
Luke Munsch <lpmunsch@gmail.com>
2023-09-21 15:29:17 +0100

    update styles and content for stripe installation

--- checkout/views.py
+++ checkout/views.py
@@ -15,5 +15,7 @@ def checkout(request):
     order_form = OrderForm()
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KfrxTCFZ5MUgwOyPW7N4O2Hi881agVemSpuiBBg5chjcX3ghQSxGCq7CpXjoy0cr96cCylYXHyC800UpA94FKTG00kE2WsDgt',
+        'client_secret': 'test client secret',
     }
     return render(request, 'checkout/checkout.html', context)
