46fc8b5e385a8a882c5ffd50370283f4112897e1
Luke Munsch <lpmunsch@gmail.com>
2023-10-26 16:22:35 +0100

    Update webhook file for emails

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -46,12 +46,8 @@ class StripeWH_Handler:
         pid = intent.id
         basket = intent.metadata.basket
         save_info = intent.metadata.save_info
-    
-        # Get the Charge object
-        stripe_charge = stripe.Charge.retrieve(
-            intent.latest_charge
-        )
-        billing_details = stripe_charge.billing_details
+
+        billing_details = intent.charges.data[0].billing_details
         shipping_details = intent.shipping
         grand_total = round(intent.charges.data[0].amount / 100, 2)
 
