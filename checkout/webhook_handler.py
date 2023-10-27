e7ad801795d58e49a10f482515a7a7f6098078de
Luke Munsch <lpmunsch@gmail.com>
2023-10-27 12:32:39 +0100

    Update webhook handler postcode issue

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -44,26 +44,18 @@ class StripeWH_Handler:
 
     def handle_payment_intent_succeeded(self, event):
         """handle successful payment intent webhooks from stripe"""
-        print("hello")
         intent = event.data.object
-        print(intent)
         pid = intent.id
-        print(pid)
         basket = intent.metadata.basket
-        print(basket)
         save_info = intent.metadata.save_info
 
         # Get the Charge object
         stripe_charge = stripe.Charge.retrieve(
             intent.latest_charge
         )
-        print(stripe_charge)
         billing_details = stripe_charge.billing_details
-        print(billing_details)
         shipping_details = intent.shipping
-        print(shipping_details)
         grand_total = round(stripe_charge.amount / 100, 2)
-        print(grand_total)
 
         # clean data in the shopping details
         for field, value in shipping_details.address.items():
@@ -73,7 +65,6 @@ class StripeWH_Handler:
         # Update profile information if save_info was checked
         profile = None
         username = intent.metadata.username
-        print(username)
         if username != 'AnonymousUser':
             profile = UserProfile.objects.get(user__username=username)
             if save_info:
@@ -85,7 +76,6 @@ class StripeWH_Handler:
                 profile.default_street_address2 = shipping_details.address.line2
                 profile.default_county = shipping_details.address.state
                 profile.save()
-                print("saved!")
 
         order_exists = False
         attempt = 1
@@ -106,14 +96,12 @@ class StripeWH_Handler:
                     stripe_pid=pid,
                 )
                 order_exists = True
-                print("exists!")
                 break
             except Order.DoesNotExist:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
             self._send_confirmation_email(order)
-            print("boom!")
             return HttpResponse(
                 content=(f'Webhook received: {event["type"]} | \
                         SUCCESS: Verified orer already in database'),
@@ -121,7 +109,6 @@ class StripeWH_Handler:
             )
         else:
             order = None
-            print("none!")
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
@@ -145,17 +132,14 @@ class StripeWH_Handler:
                         quantity=quantity,
                     )
                     order_line_item.save()
-                print(order)
             except Exception as e:
                 if order:
                     order.delete()
-                print("deleted!")
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
 
         self._send_confirmation_email(order)
-        print("confirmed!")
         return HttpResponse(
             content=(f'Webhook received: {event["type"]} | SUCCESS: '
                      'Created order in webhook'),
